441a778b2f519841e2bf7771b308c84afb7a4099
Mahtte94 <mahtias.jebrand@live.se>
2025-04-28 14:29:32 +0200

    redid placing of text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,10 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
+        <div className={styles.footerText}>
         <p>&copy; 2025 Recipe Finder. All rights reserved.</p>
-        <p>Created by Josefin, Kasper & Mahtias</p>
+          <p>Created by Josefin, Kasper & Mahtias</p>
+          </div>
       </div>
     </footer>
   );
